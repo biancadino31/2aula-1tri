e603f1008aa1a0e76d8b02072850476da471690c
biancadino31 <160597879+biancadino31@users.noreply.github.com>
2025-02-27 15:28:30 -0300

    ola
    ola

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
+    
+    
+    
+    
+    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    if (!aumentaFonteBotao || !diminuiFonteBotao) {
+        console.error("Os botões não foram encontrados no documento.");
+        return;
+    }
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+});
